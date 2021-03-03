04ec0b7b556648381fad36b8cea483a3054df627
Muratcan Akçay <muratcanakcay@gmail.com>
2021-03-03 06:44:15 +0100

    Prepared project for Lab 02

---
+++ Lab02/Xml.Console/LibraryReader.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace Xml.Console
+{
+    public static class LibraryReader
+    {
+        public static Library.library ReadLibrary(string path)
+        {
+            var serializer = new XmlSerializer(typeof(Library.library));
+            return (Library.library)serializer.Deserialize(new FileStream(path, FileMode.Open, FileAccess.Read));
+        }
+    }
+}
---
+++ Lab02/Xml.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+
+
+namespace Xml.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var libraryPath = Path.Combine(Directory.GetCurrentDirectory(), "library.xml");
+            var library = LibraryReader.ReadLibrary(libraryPath);
+
+            var authors = library.books.SelectMany(l => l.authors);
+
+            foreach (var author in authors)
+            {
+                System.Console.WriteLine($"Author: {string.Join(",", author.names.ToArray())}, {author.surname}");
+            }
+        }
+    }
+}

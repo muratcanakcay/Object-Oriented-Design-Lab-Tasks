a45f30516119af23b21965226b7aaf52cbe8fbf7
Muratcan Akçay <muratcanakcay@gmail.com>
2021-03-03 06:20:02 +0100

    Update

--- Lab01/Xml.Console/Program.cs
+++ Lab01/Xml.Console/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Xml.Console
 {
@@ -10,6 +8,15 @@ namespace Xml.Console
     {
         static void Main(string[] args)
         {
+            var libraryPath = Path.Combine(Directory.GetCurrentDirectory(), "library.xml");
+            var library = LibraryReader.ReadLibrary(libraryPath);
+
+            var authors = library.books.SelectMany(l => l.authors);
+
+            foreach (var author in authors)
+            {
+                System.Console.WriteLine($"Author: {string.Join(",", author.names.ToArray())}, {author.surname}");
+            }
         }
     }
 }

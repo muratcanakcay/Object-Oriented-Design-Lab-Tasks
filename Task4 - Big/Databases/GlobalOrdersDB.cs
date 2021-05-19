5fa355860e7a1c747a16e07a08adb43097fbf4ca
Muratcan Akcay <muratcanakcay@gmail.com>
2021-05-20 00:22:35 +0200

    Task4 - Init

---
+++ Task4 - Big/Databases/GlobalOrdersDB.cs
@@ -0,0 +1,28 @@
+﻿using OrderProcessing.Orders;
+
+namespace OrderProcessing.Databases
+{
+    public class GlobalOrdersDB
+    {
+        public OrderNode Root { get; private set; }
+
+        public GlobalOrdersDB(OrderNode root)
+        {
+            Root = root;
+        }
+    }
+    public class OrderNode
+    {
+        public OrderNode Parent { get; }
+        public OrderNode Left { get; }
+        public OrderNode Right { get; }
+        public Order Order { get; }
+
+        public OrderNode(OrderNode left, OrderNode right, Order order)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.Order = order;
+        }
+    }
+}
---
+++ Task4 - Big/Shipment/LabelFormatter.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace OrderProcessing.Shipment
+{
+    public interface ILabelFormatter
+    {
+        string GenerateLabelForOrder(string providerName, IAddress address);
+    }
+}
